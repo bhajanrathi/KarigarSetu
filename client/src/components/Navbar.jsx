1c6ac3291745e418ae52b13fdeb052248159af8d
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2025-04-16 17:47:33 +0530

    added assets folder

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { NavLink, useNavigate } from "react-router-dom";
+import { assets } from '../assets/assets_frontend/assets';
 const Navbar = () => {
 
   const navigate = useNavigate();
 
 
   return (
+
+
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      <ul className="hidden md:flex items-start gap-5 font-medium">
+      <ul className="hidden md:flex  gap-5 font-medium items-center">
+
+       <img src={assets.logo} alt=""  onClick={() => {
+          navigate("/");
+        }}
+        className="w-30" />
+
         <NavLink to="/">
           <li className="py-1">Home</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
