6c3ab84f0358061b7f8e94504662ade514f2697b
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2025-04-16 17:24:22 +0530

    added all the files and routes

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink, useNavigate } from "react-router-dom";
+const Navbar = () => {
+
+  const navigate = useNavigate();
+
+
+  return (
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <NavLink to="/">
+          <li className="py-1">Home</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/doctors">
+          <li className="py-1">All Products</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto  hidden" />
+        </NavLink>
+        <NavLink to="/about">
+          <li className="py-1">About</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/contact">
+          <li className="py-1">Contact</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/Myprofile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Myprofile = () => {
+  return (
+    <div>
+      i am profile 
+    </div>
+  )
+}
+
+export default Myprofile
