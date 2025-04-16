5f6f8f32fb133163e2bf6066448fdf406db00382
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2025-04-16 14:44:59 +0530

    tailwind used

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>hiii i am app.js</div>;
+  return <div className="text-green-800 bg-blue-700">hiii i am app.js</div>;
 };
 
 export default App;
